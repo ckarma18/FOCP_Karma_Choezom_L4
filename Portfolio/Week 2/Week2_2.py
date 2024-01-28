e435fbc3639936e2877bd978ea8f5d92971b3d17
ckarma18 <136043675+ckarma18@users.noreply.github.com>
2024-01-28 16:57:16 +0545

    Delete Portfolio directory

--- Portfolio/Week 2/Week2_2.py
+++
@@ -1,8 +0,0 @@
-'''write a program that prompts a user to enter a temperature in Celsius, and then
-displays the corresponding temperature in Fahrenheit, like so:
-Enter a temperature in Celsius: 32.5
-32.5C is equivalent to 90.5F'''
-
-c = float(input('Input the temperature in celsius: '))
-f = c * (9 / 5) + 32
-print(f'{c}C is equivalent to {f}F')
--- Portfolio/Week 3/Week3_3.py
+++
@@ -1,9 +0,0 @@
-""" Modify your previous program so that the password must be between 8 and 12
-characters (inclusive) long."""
-
-passw1=input("enter new pass word: ")
-passw2=input("enter password: ")
-if passw1==passw2 and len(passw1)>=8 and len(passw2)>=12:
-    print("password set")
-else:
-    print("error") 
